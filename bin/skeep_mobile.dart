f85098d1efa43424f10713aa1dec35ee87e91802
felipegarciamf <silas.garcia@mfconsulting.com.br>
2024-01-25 11:31:21 -0300

    estudando tipos e uso de variaveis em dart

---
+++ bin/skeep_mobile.dart
@@ -0,0 +1,20 @@
+void main(List<String> arguments){
+
+
+  int id = 1;
+  int id2 = 2;
+  bool isTrue = false;
+  print('Hello world: ${id}!');
+  print('Hello world: $isTrue!');
+
+  List<String> teste = [ 'a', 'b', 'c' ];
+
+  print(teste);
+
+  List<dynamic> teste2 = [ 'a', 'b', 'c', 1, 2, 3, true, false ];
+
+  print(teste2);
+
+
+
+}
