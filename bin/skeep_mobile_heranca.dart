e5144de2cc4acaf85bcfabcbc9da1dae1d79b438
felipegarciamf <silas.garcia@mfconsulting.com.br>
2024-01-26 19:00:47 -0300

    criando herança e usando o super

--- bin/skeep_mobile_heranca.dart
+++ bin/skeep_mobile_heranca.dart
@@ -1,9 +1,21 @@
-class Legumes extends Alimentos {
+class Legumes extends Aliemntos {
   String nome;
   Legumes(this.nome, String tipo) : super(tipo);
+
+
+  estaMadura(int dias) {
+    return dias > 30;
+  }
 }
 
 class Aliemntos {
   String tipo;
   Aliemntos(this.tipo);
+}
+
+void main(List<String> arguments) {
+  Legumes legumes = Legumes("cenoura", "legume");
+  print(legumes.nome);
+  print(legumes.tipo);
+  print(legumes.estaMadura(31));
 }
